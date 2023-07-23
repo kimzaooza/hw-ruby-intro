175faf94213510a24dbf2504fdcc861d04bd9be0
Theethawach W <99662035+kimzaooza@users.noreply.github.com>
2023-07-23 19:45:23 +0000

    intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,64 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+    end
+      arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+    end
+    arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  cpms = {}
+  arr.each do |em|
+    cpm = n - em
+    return true if cpms[cpm]
+    cpms[em] = true
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+ if s[0] =~ /[b-df-hj-np-tv-z]/i
+    return true
+  else return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s.match?(/^[01]+$/)
+  number = s.to_i(2)
+  number % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN number cannot be empty' if isbn.empty?
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', price)
+  end
 end
